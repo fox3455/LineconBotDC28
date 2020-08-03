c0879087650e55e5375ed9332bf99133c6716f29
Alex <alex@2a03.party>
2020-08-03 13:25:27 -0500

    I dont even know where to begin describing this but this is me showing my progress.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 // Load up the libraries
 const Discord = require('discord.js');
-const prettyMilliseconds = require('pretty-ms');
 
 // Importing config
 const config = require('./config.json')
 
-//User array and random user picker
-var randomValue = myArray[Math.floor(Math.random() * myArray.length)];
-var players = []
-var value = players[Math.floor(Math.random() * myArray.length)];
-
+//User array and random number
+let players = [];
+let value = players[Math.floor(Math.random() * players.length)];
 
 // Create client
 const client = new Discord.Client();
+
+// Filter for filtering out only the beach ball emote
+const filter = (reaction) => {
+	return reaction.emoji.name === '739737953181630584';
+};
 
 client.on('ready', () => {
 	//logs when the bot comes online
@@ -30,41 +32,44 @@ client.on('ready', () => {
 	}).then(() => {
 	});
 
-	// Collects messages and reactions?
-	const filter = (reaction) => {
-		return reaction.emoji.name === '739737953181630584';
-	};
-	const collector = message.channel.createReactionCollector(filter, { time: 15000 });
 })
 
-client.on('message', () => {
-	if (message.channel === 710574818222931968) {
-		//Game function
-		if (message.content === '!play') {
-			message.reply("Who wants to play with the beach ball?");
-			message.react('739737953181630584');
-			// going to change this to an on. statement and try making it more "JS" I think I see where you are going.
-			message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-				.then(collected => {
-					const reaction = collected.first();
-					if (reaction.emoji === '739737953181630584') {
-						players = reaction.users;
-					}
-				})
-      //This will start passing the ball
-      let ball = message.reply("739737953181630584 <@" + value + ">") + message.react('739774044974481478')
+client.on('message', async message => {
+	if ((message.channel === 710574818222931968)) return;
+	//Game function
+	if (message.content === '!play') {
+		message.reply("Who wants to play with the beach ball?").then(() => {
+			message.react('739737953181630584')
+		})
+			.catch((err) => {
+					console.error("Something went wrong!", err)
+					return message.channel.send("Something went wrong..."
+					)
+				}
+			)
+		;
+		// going to change this to an on. statement and try making it more "JS" I think I see where you are going.
+		message.awaitReactions(filter, {max: 1, time: 60000, errors: ['time']})
+			.then(collected => {
+				const reaction = collected.first();
+				if (reaction.emoji === filter) {
+					players = reaction.users;
+				}
+			})
+		//This will start passing the ball
+		let ball = await message.reply("<:BeachBall1:737676787823411230> <@" + value + ">") + message.react('737676787823411230')
 
-      message.reply("Let's start!")
-      message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
-				.then(collected => {
-					const reaction = collected.first();
-					if (reaction.emoji === '739774044974481478') {
+		message.reply("Let's start!").then(() => {
+		})
+		message.awaitReactions(filter, {max: 1, time: 30000, errors: ['time']})
+			.then(collected => {
+				const reaction = collected.first();
+				if (reaction.emoji === message.guild.cache.find(emoji => emoji.id('737676787823411230'))) {
 
-
-					}
-				})
-		}
+				}
+			})
 	}
+
 });
 
 client.login(config.token);
